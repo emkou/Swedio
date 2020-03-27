2a662731a3c181038594e8fbb3100204a8072b08
Emil <emil@doychinov.com>
2020-03-27 20:58:31 +0100

    Initial Commit

---
+++ Swedio WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Swedio WatchKit Extension
+//
+//  Created by Emil Doychinov on 2020-03-27.
+//  Copyright © 2020 Emil Doychinov. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
